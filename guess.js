7514d0e26b9b0bb0542cba45b8e437dc3a1efadf
Fanessa Hilaire <fan@Fanessas-MacBook-Air.local>
2022-10-13 09:35:46 -0400

    second

---
+++ guess.js
@@ -0,0 +1,9 @@
+/*
+ create a program that bgeuss a number from 1 to 10 
+ then aks us to guess the number.
+if it is higher say "too high" and ask again.again
+if it is lower say "too low" and ask again .
+if it is the right number say "congratulations "
+*/
+
+import readline from "readline-sync"  
