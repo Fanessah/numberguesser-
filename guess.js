a637c337542f5d42ff9cd9a6981ff9eaef5ec3d1
Fanessa Hilaire <fan@Fanessas-MacBook-Air.local>
2022-10-13 10:38:04 -0400

    second commit

--- guess.js
+++ guess.js
@@ -7,3 +7,37 @@ if it is the right number say "congratulations "
 */
 
 import readline from "readline-sync"  
+
+let userGuessedRight = false
+
+
+    //  Guess a number from 1-10
+    let numberChosen = Math.floor(Math.random() * 10)
+    // console.log(numberChosen)
+
+    while(!userGuessedRight) {
+        let guess = readline.question("what is your guess?")
+        
+        if (guess == numberChosen) //
+        {
+            console.log("congratulations")
+            userGuessedRight = true
+        }
+        if (guess < numberChosen)
+        {
+            console.log("too low")
+            userGuessedRight = false
+        }
+        if (guess > numberChosen)
+        {
+            console.log("too high")
+            userGuessedRight = false
+        }
+    // console.log(guess)
+
+    }
+    
+while(!userGuessedRight) {
+    console.log ("what is your guess?")
+
+ } 
